c209b827f9736353190cb94bbb6685b33a403735
EzraEzeiru <ezeiru.ezra@gmail.com>
2022-11-29 00:18:30 +0100

    Updated Cors

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     cors: {
-      origin: 'https://b54-frontend-web.vercel.app/#/',
+      origin: 'https://b54-frontend-web-ezeiruezra-ezra-test.vercel.app/#/',
       methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
       preflightContinue: false,
       optionsSuccessStatus: 204,
