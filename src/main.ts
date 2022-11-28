9b24e577015a3319b60ec65da32ffea1212c96a8
EzraEzeiru <ezeiru.ezra@gmail.com>
2022-11-28 16:24:18 +0100

    added procfile

--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,6 @@ async function bootstrap() {
       optionsSuccessStatus: 204,
     },
   });
-  await app.listen(process.env.PORT || '0.0.0.0');
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
